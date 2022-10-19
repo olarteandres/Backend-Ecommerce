e548935922293e76676c8161bfa633021d0fa1ba
olarteandres <andresolarte31@gmail.com>
2022-10-19 12:04:16 -0300

    change

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('API')
-    .setDescription('PLATZI STORE')
+    .setDescription('Backend Ecommerce')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
